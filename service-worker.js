849ce8e7339e2d5af371d429d4a2a4d5e4820dc5
SANNE-gif <rahimsanne743@gmail.com>
2025-11-12 22:31:14 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'led-control-v1';
+const urlsToCache = [
+  '/LedControl/',
+  '/LedControl/index.html'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activation du Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Suppression ancien cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
